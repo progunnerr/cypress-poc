0c9cc22c8ff7a39fdc1864ba3850ac9f01edd834
Hartej <hsgrewal687@gmail.com>
2024-12-19 16:32:29 -0500

    feat: github workflow file and mochawesom-reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    chart: false,
+    reportPageTitle: 'Cypress Test Results',
+    embeddedScreenshots: false,
+    inlineAssets: false,
+    saveAllAttempts: false,
+    overwrite: false,
+    saveJson: true,
+    html: false,
+    json: true,
+    reportDir: 'cypress/reports',
+  },
   e2e: {
     // Configure your E2E tests here
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}"
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
+    setupNodeEvents(on, config){
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+    screenshotOnRunFailure: true,
   },
 })
